49c2b558a1f5306847b9b6814dde9077afa4fd70
Soumya-CET <soumyaranjanmohanty.cetb@gmail.com>
2021-01-30 23:53:18 +0530

    1st commit

---
+++ chbt.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import request
+import requests
+import json
+
+app = Flask(__name__)
+
+headers = {'Content-Type':'application/json', 'Authorization':'Bearer NTVlYTY2NGQtMDM3ZC00OTUzLThmNTctNWY1NDc0ZmMwNjYxMWVmZWU2ZjktYTIw_PF84_consumer'}
+
+@app.route('/reply', methods=['GET', 'POST'])
+def reply():
+    if request.method == 'POST':
+        id = request.json["data"]["id"]
+        mess_rcv, sender_email = get_message(id)
+        res = send_message(mess_rcv, sender_email)
+        return res
+
+def get_message(id):
+    url = "https://webexapis.com/v1/messages/" + id
+    payload = {}
+    response = requests.request("GET", url,headers=headers, data = payload)
+    message = json.loads(response.text)
+    print(message)
+    return message["text"], message["personEmail"]
+
+def send_message(message,email):
+    url = "https://webexapis.com/v1/messages"
+    reply = ""
+    if (message.lower() in ["test", "testing"]):
+        reply = "ACK"
+    elif (message.lower() in ["hi", "hello"]):
+        reply = "Hello there!"
+    else:
+        reply = "Did you say " + message + " ?"
+
+    payload = '''{"toPersonEmail":"'''+email+'''","text":"'''+reply+'''"}'''
+    print(payload)
+    response = requests.request("POST", url, headers = headers, data = payload)
+    print(response)
+    return response.text
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port = "5500")
